a644e9ac42f6716e8706b163439cbe032e35621e
jamshidakramovich14 <jamshidturdiyev761@gmail.com>
2024-04-19 14:40:07 +0500

    lesson 14 homework

---
+++ script.js
@@ -0,0 +1,128 @@
+"use strict"
+
+
+// ------------------- String Length -----------------------
+
+
+// ------------------ 1-masala start ------------------
+
+// function getLength(string) {
+//     return string.length;
+// }
+
+// let name = prompt("Biron bir so'z kiriting")
+// console.log(getLength(name));
+
+// ------------------ 1-masala end ------------------
+
+
+// ------------------- String Length -----------------------
+
+
+
+
+
+
+// ------------------- String Comparison --------------------
+
+// ------------------ 1-masala start ------------------
+
+// function tenglikniToping(string1, string2) {
+// string1 = "Izzatillo";
+// string2 = "Izzatillo";
+
+// console.log(string1 === string2);
+// }
+
+// tenglikniToping();
+
+// ------------------ 1-masala end ------------------
+
+// ------------------- String Comparison --------------------
+
+
+
+
+
+
+// ---------------------- String Iteration -----------------------
+
+// ------------------ 1-masala start ------------------
+
+// function getLength(string){
+// for (let i = 0; i < string.length; i++){
+//     console.log(string[i]);
+// }
+// }
+
+// getLength("I am string");
+
+
+// ------------------ 1-masala end ------------------
+
+// ---------------------- String Iteration -----------------------
+
+
+
+
+// ----------------- Slicing and Substring -----------------
+
+// ------------------ 1-masala end ------------------
+
+// function sliceWord(words) {
+//     let n = words.split(" ");
+//     return n[n.length - 1];
+
+// }
+// console.log(sliceWord("String"));
+
+// ------------------ 1-masala end ------------------
+
+// ----------------- Slicing and Substring -----------------
+
+
+
+// ---------------- Uppercase an lowercase ----------------
+
+// ------------------ 1-masala start ------------------
+
+// function changeToUpperCase(string) {
+//     return string.toUpperCase();
+//   }
+
+// const result = changeToUpperCase("Hello World");
+// console.log(result);
+
+// ------------------ 1-masala end ------------------
+
+
+// ------------------ 2-masala satrt ------------------
+
+// function changeToLowerCase(string) {
+//     return string.toLowerCase();
+// }
+
+// const result = changeToLowerCase("Goodbye World");
+// console.log(result);
+
+// ------------------ 2-masala end ------------------
+
+// ---------------- Uppercase an lowercase ----------------
+
+
+
+
+// ------------------- Replacing text -------------------
+
+// ------------------ 1-masala start ------------------
+
+// function replaceValue(string, oldValue, newValue) {
+//     return string.replace(oldValue, newValue);
+//   }
+
+//   const result = replaceValue("Hello World", "Hello", "World");
+//   console.log(result);  
+
+// ------------------ 1-masala end ------------------
+
+// ------------------- Replacing text -------------------
